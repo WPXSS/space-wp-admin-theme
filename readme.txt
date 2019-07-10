5de2fa282f08d8765711db38701fc1d0a21c65f2
Stefan Pejcic <pejcha1994@gmail.com>
2019-07-10 14:26:38 +0200

    initial
    the version from wp.org

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Space Admin Theme ===
+Contributors: StefanPejcic
+Tags: admin, admin page, admin panel, admin theme style plugin, wordpress admin panel, admin-theme, backend theme, custom admin theme, flat admin theme, free admin theme style plugin, modern admin theme, new admin ui, simple admin theme, white Label, wordpress, wordpress admin dashboard, wordpress admin theme, wp admin page, wp admin theme, space, stefan pejcic, clean, wp admin
+Donate link: http://giga.rs
+Requires at least: 4.0
+Tested up to: 5.2
+Stable tag: 5.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Brings a nice, modern look to the WordPress admin.
+
+== Description ==
+
+WordPress admin theme that brings a nice and modern look to your wp-admin. Change is good! :)
+
+If you like the Space Admin Theme, please consider leaving a review here on WordPress. :)
+
+== Credits ==
+based on ShiftShort: https://github.com/short/shift-short-wp-admin-theme
+
+Icons by Elegant Themes: http://www.elegantthemes.com/blog/resources/elegant-icon-font
+
+== Installation ==
+
+FROM WP ADMIN: Install from the WordPress.org respository by going to Plugins > Add New and search for Space Admin Theme. Install & Activate.
+BY UPLOADING: Upload and extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+
+== Frequently Asked Questions ==
+
+= How to change color scheme? =
+
+From your User > Edit Profile page
+
+= How to open the Rocket Bar Search? =
+
+Shift + Space
+
+== Screenshots ==
+
+1. Admin Dashboard
+2. Color Schemes
+3. Rocket Bar Search
+4. Login Page
+
+== Changelog ==
+
+= 1.0 =
+Initial
---
+++ space-admin-theme.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Space Admin Theme
+Plugin URI: https://giga.rs/space-wp-admin-theme/
+Description: Modern looking WordPress admin theme for the dashboard.
+Version: 1.0
+Author: Stefan Pejcic
+Author URI: https://giga.rs/
+License: GPLv2
+*/
+/*
+ *      Copyright 2015 Northon Torga @Shift Empreendimentos Virtuais <producao@shift.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+?>
+<?php
+
+// LOGIN PAGE
+add_action( 'login_enqueue_scripts', 'spacewpat_theme_login' );
+function spacewpat_theme_login() {
+    wp_enqueue_style( 'spacewpat-login', plugins_url('css/login.css', __FILE__) );
+}
+
+add_filter( 'login_headerurl', 'spacewpat_login_url_logo' );
+function spacewpat_login_url_logo() {
+    return home_url();
+}
+
+add_filter( 'login_headertitle', 'spacewpat_login_logo_url_title' );
+function spacewpat_login_logo_url_title() {
+    return the_title();
+}
+
+
+// WP ADMIN
+add_action( 'admin_enqueue_scripts', 'spacewpat_theme_general' );
+function spacewpat_theme_general() {
+        wp_enqueue_style( 'spacewpat-general', plugins_url('css/general.css', __FILE__) );
+}
+
+// WP ADMIN BAR
+add_action( 'wp_head', 'spacewpat_theme_bar' );
+function spacewpat_theme_bar() {
+	if ( is_user_logged_in() ) {
+        wp_enqueue_style( 'spacewpat-bar', plugins_url('css/admin-bar.css', __FILE__) );
+	}
+}
